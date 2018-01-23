463088857a9e4bc1de441fc5eb197effbd449e32
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-23 09:30:47 +0100

    Ready Task

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,57 @@
-function Phone(model, brand, price, color, screen, procesor, operating_system) {
-	this.model = model;
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-	this.screen = screen;
-	this.procesor = procesor;
-	this.operating_system = operating_system;
+function PhonesFactory() {
+  this.phonesList = {};
 }
 
-var phoneList = {};
-function addPhonename(){
-	
-}
+PhonesFactory.prototype.createInstance = function(phone) {
+  function Phone(model, brand, price, color) {
+    this.model = model;
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+  }
 
-Phone.prototype.printInfo = function(){
-	var screenResolution = this.screen.resolution;
-	var diagonal = this.screen.diagonal;
-	var contrast = this.screen.displayContrast;
-	console.log("The phone brand is " + this.brand + " ,color is: " + this.color + ", price is " + this.price + " zł." + screenResolution + " " + contrast);
+  var instance = new Phone(phone[0], phone[1], phone[2], phone[3]);
+  this.addToList(instance);
 };
 
+PhonesFactory.prototype.addToList = function(phone){
+  this.phonesList[phone.model] = phone;
 
-var iPhone8Plus = new Phone("IPhone8Plus", "Apple", 3979, "gold", {resolution: '1920 x 1080', diagonal: 5.5, displayContrast: 1300},  'A11 Bionic', 'iOS 11');
-var galaxyS8 = new Phone("GalaxyS8", "Samsung", 2742.99, "orchid grey", {resolution: '2220 x 1080', diagonal: 5.6, displayContrast: 'brak danych'}, 'ośmiordzeniowy, taktowanie 2,2 GHz', 'Android');
-var xperiaXZ1 = new Phone("XperiaYZ1", "Sony", 2999, "księżycowy błekit", {resolution: 'brak danych', diagonal: 5.2, displayContrast: 'Dynamiczne poprawianie kontrastu'}, ' Qualcomm® Snapdragon™ 835 Mobile Platform', 'Google Android™ O');
+  console.log(this.phonesList);
+  return this.phonesList;
+};
 
-iPhone8Plus.printInfo();
-galaxyS8.printInfo();
-xperiaXZ1.printInfo();
+PhonesFactory.prototype.printPhone = function(model){
+	console.log(this.phonesList[model]);
+}
+
+
+var phone1 = ["IPhone8Plus", "Apple", 3979, "gold"];
+var phone2 = ["GalaxyS8", "Samsung", 2742.99, "orchid grey"];
+var phone3 = ["XperiaYZ1", "Sony", 2999, "księżycowy błekit"];
+
+var factory1 = new PhonesFactory(phone1);
+
+factory1.createInstance(phone1);
+factory1.createInstance(phone2);
+factory1.createInstance(phone3);
+
+/*var phone = 'GalaxyS8';*/
+factory1.printPhone('GalaxyS8'); 
+
+
+
+/*printPhone(phoneModel);*/
+
+/*PhonesFactory.prototype.printInfo = function(phonesList){
+	
+
+};*/
+
+/*iPhone8Plus.printInfo();*/
+/*galaxyS8.printInfo();*/
+/*xperiaXZ1.printInfo();*/
+
+
+
+
